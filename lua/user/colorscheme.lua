2725f6a165b679024afa6420e748d15951431c37
Michal Svorc <dev@michalsvorc.com>
2022-12-18 20:09:47 +0100

    feat(colorscheme): use tokyonight storm

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,5 @@
-local colorscheme = "tokyonight-night"
+local tokyonight = "tokyonight-storm"
+local colorscheme = tokyonight
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
