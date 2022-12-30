b966ceaef837dd3ca3085d467be7d41de5b36eef
Michal Svorc <dev@michalsvorc.com>
2022-12-30 21:27:00 +0100

    feat(options): use rg for :grep if available

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -41,3 +41,8 @@ vim.opt.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the p
 vim.opt.iskeyword:append("-")                   -- treats words with `-` as single words
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
+
+-- Use faster grep alternatives if possible
+if vim.fn.executable('rg') == 1 then
+  vim.opt.grepprg = 'rg --vimgrep'
+end
