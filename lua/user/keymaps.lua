c57db831da70e158facb9a1b9ddc77eb7739b1a4
Michal Svorc <m.svorc@sportradar.com>
2023-01-03 22:34:34 +0100

    feat(nnn-nvim): explore keymao, set nnn functions

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -103,4 +103,5 @@ keymap({"n", "x", "o"}, "<leader>X", "<Plug>(leap-backward-till)")
 keymap({"n", "x", "o"}, "<leader>gs", "<Plug>(leap-cross-window)")
 
 --- nnn.nvim
+keymap({"n", "t"}, "<leader>e", "<cmd>NnnExplorer<cr>", opts)
 keymap({"n", "t"}, "<leader>fe", "<cmd>NnnPicker<cr>", opts)
