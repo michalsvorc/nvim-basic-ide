ea539d7238176d221cd717632aa0bd9481dad4f9
Michal Svorc <dev@michalsvorc.com>
2023-08-26 11:11:48 +0200

    chore(nvim-tree): update to 0074120

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,6 +1,6 @@
 local M = {
   "kyazdani42/nvim-tree.lua",
-  commit = "f5d970d4506f385b29534252d8c15a782fa53034",
+  commit = "00741206c2df9c4b538055def19b99790f0c95c8",
   event = "VimEnter",
 }
 
