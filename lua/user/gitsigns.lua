260760c11cac55afa2331983e30b7d34a3193234
Michal Svorc <dev@michalsvorc.com>
2023-08-26 15:52:21 +0200

    chore(gitsigns): update to 79127db

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -1,6 +1,6 @@
 local M = {
   "lewis6991/gitsigns.nvim",
-  commit = "ec4742a7eebf68bec663041d359b95637242b5c3",
+  commit = "79127db3b127f5d125f35e0d44ba60715edf2842",
   event = "BufReadPre",
 }
 
